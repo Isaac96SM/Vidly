e162c716e4acfd1e1bbb1be5e9946bab7a851ab1
Isaac96SM <suarezmota@gmail.com>
2017-03-14 23:54:52 +0100

    section 2

---
+++ WebApplicationVidly/WebApplicationVidly/Controllers/CustomersController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplicationVidly.Models;
+
+namespace WebApplicationVidly.Controllers
+{
+    public class CustomersController : Controller
+    {
+        // GET: Customers
+        public ActionResult Index()
+        {
+            var customers = new List<Customer>
+            {
+                new Customer { Id = 0, Name = "Isaac Suárez" },
+                new Customer { Id = 1, Name = "Judith Raïss" }
+            };
+
+            return View(customers);
+        }
+    }
+}
--- WebApplicationVidly/WebApplicationVidly/Controllers/MoviesController.cs
+++ WebApplicationVidly/WebApplicationVidly/Controllers/MoviesController.cs
@@ -4,37 +4,49 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using WebApplicationVidly.Models;
+using WebApplicationVidly.ViewModels;
 
 namespace WebApplicationVidly.Controllers
 {
     public class MoviesController : Controller
     {
+        public ActionResult Index()
+        {
+            var movies = new List<Movie>
+            {
+                new Movie { Id = 0, Name = "Shrek" },
+                new Movie { Id = 1, Name = "Wall-e" }
+            };
+
+            return View(movies);
+        }
+
         // GET: Movies/Random
         public ActionResult Random()
         {
             var movie = new Movie() { Name = "Shrek!" };
+            var customers = new List<Customer>
+            {
+                new Customer { Name = "Customer 1" },
+                new Customer { Name = "Customer 2" }
+            };
 
-            //return View(movie);
+            var viewModel = new RandomMovieViewModel
+            {
+                Movie = movie,
+                Customers = customers
+            };
+
+            return View(viewModel);
             //return Content("Hello World");
             //return HttpNotFound();
             //return new EmptyResult();
-            return RedirectToAction("Index", "Home", new { page = 1, sortBy = "name" });
+            //return RedirectToAction("Index", "Home", new { page = 1, sortBy = "name" });
         }
 
-        public ActionResult Edit(int id)
+        public ActionResult ByReleaseDate(int year, int month)
         {
-            return Content("id=" + id);
-        }
-
-        public ActionResult Index(int? pageIndex, string sortBy)
-        {
-            if (!pageIndex.HasValue)
-                pageIndex = 1;
-
-            if (String.IsNullOrWhiteSpace(sortBy))
-                sortBy = "Name";
-
-            return Content(String.Format("pageIndex={0}&sortBy={1}", pageIndex, sortBy));
+            return Content(year + "/" + month);
         }
     }
 }
