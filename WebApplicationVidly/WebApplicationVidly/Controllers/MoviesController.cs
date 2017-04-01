d6d932b7fa6a57eaef8b63d8009b2c7ff829e986
Isaac96SM <suarezmota@gmail.com>
2017-04-01 17:11:02 +0200

    ejercicio seccion 4

--- WebApplicationVidly/WebApplicationVidly/Controllers/MoviesController.cs
+++ WebApplicationVidly/WebApplicationVidly/Controllers/MoviesController.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Data.Entity;
+using System.Data.Entity.Validation;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WebApplicationVidly.Migrations;
 using WebApplicationVidly.Models;
 using WebApplicationVidly.ViewModels;
 
@@ -38,6 +40,55 @@ namespace WebApplicationVidly.Controllers
                 return HttpNotFound();
 
             return View(movie);
+        }
+
+        public ActionResult New()
+        {
+            var genres = _context.Genres.ToList();
+            var viewModel = new MovieFormViewModel
+            {
+                Genres = genres
+            };
+
+            return View("MovieForm", viewModel);
+        }
+
+        public ActionResult Edit(int id)
+        {
+            var movie = _context.Movies.SingleOrDefault(c => c.Id == id);
+
+            if (movie == null)
+                return HttpNotFound();
+
+            var viewModel = new MovieFormViewModel
+            {
+                Movie = movie,
+                Genres = _context.Genres.ToList()
+            };
+
+            return View("MovieForm", viewModel);
+        }
+
+        [HttpPost]
+        public ActionResult Save(Movie movie)
+        {
+            if (movie.Id == 0)
+                _context.Movies.Add(movie);
+            else
+            {
+                var movieInDb = _context.Movies.Single(c => c.Id == movie.Id);
+
+                //TryUpdateModel(customerInDb, "", new string[] { "Name", "Email" });
+
+                movieInDb.Name = movie.Name;
+                movieInDb.ReleaseDate = movie.ReleaseDate;
+                movieInDb.GenreId = movie.GenreId;
+                movieInDb.Stock = movie.Stock;
+            }
+
+            _context.SaveChanges();
+
+            return RedirectToAction("Index", "Movies");
         }
     }
 }
--- WebApplicationVidly/WebApplicationVidly/Models/Movie.cs
+++ WebApplicationVidly/WebApplicationVidly/Models/Movie.cs
@@ -14,10 +14,16 @@ namespace WebApplicationVidly.Models
         [StringLength(255)]
         public string Name { get; set; }
 
-        [Required]
         public Genre Genre { get; set; }
+
+        [Display(Name = "Genre")]
+        [Required]
         public byte GenreId { get; set; }
+
+        [Display(Name = "Release Date")]
         public DateTime ReleaseDate { get; set; }
+
+        [Display(Name = "Number in Stock")]
         public int Stock { get; set; }
     }
 }
