facd2402fd904f40ef5264ac293611764a406d11
Isaac96SM <suarezmota@gmail.com>
2017-04-01 15:02:15 +0200

    ejercicios seccion 3

--- WebApplicationVidly/WebApplicationVidly/Models/Movie.cs
+++ WebApplicationVidly/WebApplicationVidly/Models/Movie.cs
@@ -2,13 +2,22 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations;
 
 namespace WebApplicationVidly.Models
 {
     public class Movie
     {
         public int Id { get; set; }
+
+        [Required]
+        [StringLength(255)]
         public string Name { get; set; }
 
+        [Required]
+        public Genre Genre { get; set; }
+        public byte GenreId { get; set; }
+        public DateTime ReleaseDate { get; set; }
+        public int Stock { get; set; }
     }
 }
