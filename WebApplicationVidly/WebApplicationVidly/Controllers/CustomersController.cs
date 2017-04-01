45a0198d2e971a0ff9d10166a5a623df1aae20cd
Isaac96SM <suarezmota@gmail.com>
2017-04-01 19:07:26 +0200

    seccion 5

--- WebApplicationVidly/WebApplicationVidly/Controllers/CustomersController.cs
+++ WebApplicationVidly/WebApplicationVidly/Controllers/CustomersController.cs
@@ -28,6 +28,7 @@ namespace WebApplicationVidly.Controllers
             var membershipTypes = _context.MembershipTypes.ToList();
             var viewModel = new CustomerFormViewModel
             {
+                Customer = new Customer(),
                 MembershipTypes = membershipTypes
             };
 
@@ -35,8 +36,18 @@ namespace WebApplicationVidly.Controllers
         }
 
         [HttpPost]
+        [ValidateAntiForgeryToken]
         public ActionResult Save(Customer customer)
         {
+            if (!ModelState.IsValid)
+            {
+                var viewModel = new CustomerFormViewModel
+                {
+                    Customer = customer,
+                    MembershipTypes = _context.MembershipTypes.ToList()
+                };
+                return View("CustomerForm", viewModel);
+            }
             if (customer.Id == 0)
                 _context.Customers.Add(customer);
             else
--- WebApplicationVidly/WebApplicationVidly/Models/Customer.cs
+++ WebApplicationVidly/WebApplicationVidly/Models/Customer.cs
@@ -10,13 +10,14 @@ namespace WebApplicationVidly.Models
     {
         public int Id { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "Please enter customer's name")]
         [StringLength(255)]
         public string Name { get; set; }
 
         public bool IsSubscribedToNewsletter { get; set; }
 
         [Display(Name = "Date of Birth")]
+        [Min18YearsIfAMember]
         public DateTime? BirthDate { get; set; }
 
         public MembershipType MembershipType { get; set; }
