6e85c6e03dfdd0708f1aeaeefdf26d32b443f6ce
Isaac96SM <suarezmota@gmail.com>
2017-04-01 20:56:22 +0200

    seccion 6

---
+++ WebApplicationVidly/WebApplicationVidly/App_Start/MappingProfile.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using AutoMapper;
+using WebApplicationVidly.Dtos;
+using WebApplicationVidly.Models;
+
+namespace WebApplicationVidly.App_Start
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            Mapper.CreateMap<Customer, CustomerDto>();
+            Mapper.CreateMap<CustomerDto, Customer>();
+        }
+    }
+}
